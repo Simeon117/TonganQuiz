0985158efd430ffddedfd0c68663255d6cfda43d
SimeonPres <84358121+Simeon117@users.noreply.github.com>
2022-08-04 21:52:05 +0000

    quiz attemmpt

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ class Welcome(tk.Frame):
         self.exit_button = tk.Button(self, text="Back", font=("Arial", 15), bg="red", command=lambda: app.destroy())
         self.exit_button.place(x=100, y=450)
         
-
-
+global question_number
+question_number = 0
 class TonganQuiz(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -172,23 +172,119 @@ class TonganQuiz(tk.Frame):
           ,5],
         }
         
-        self.configure(bg='smoke white')
+        self.configure(bg='white smoke')
         
-        self.app_label = tk.Label(self, text="Test your language Knowldege. \n Happy Tongan language week!!", bg = "smoke white", font=("Arial Bold", 25))
-        self.app_label.place(x=40, y=150)
-
-        
-
-
-      
+        self.app_label = tk.Label(self, text="Test your language Knowldege. \n Happy Tongan language week!!", bg = "white smoke", font=("Arial Bold", 25))
+        self.app_label.place(x=40, y=150)      
         
         self.home_button = tk.Button(self, text="Home", font=("Arial", 15), command=lambda: controller.show_frame(Open))
         self.home_button.place(x=650, y=450)
         
         self.back_button = tk.Button(self, text="Back", font=("Arial", 15), command=lambda: controller.show_frame(Welcome))
         self.back_button.place(x=100, y=450)
+
         
         
+        #widgets for the quiz goes below
+        self.question_label = tk.Label (self, text = self.tongan_questions[question_number][0], font = ("Helvetica","18","bold"), bg="white smoke")
+        self.question_label.place(x=40, y=200) 
+        background_color = "white smoke"
+
+        radiobutton_value = tk.IntVar() #holds value of radio buttons
+
+        #first radio button to hold first choice answer 
+        
+        #Radiobutton 1
+        self.choice1 = tk.Radiobutton (self, text = self.tongan_questions[question_number][1], font = ("Helvetica", "14"), bg = background_color, value = 1, variable = radiobutton_value, padx = 10, pady = 10)
+        
+        self.choice1.place(x=40, y=250)
+
+        #Radiobutton 2
+        
+        self.choice2 = tk.Radiobutton (self, text = self.tongan_questions[question_number][2], font = ("Helvetica", "14"), bg = background_color, value = 2, variable = radiobutton_value, padx = 10, pady = 10)
+
+        self.choice2.place(x=40, y=300)
+
+        #Radiobutton 3
+        self.choice3 = tk.Radiobutton (self, text = self.tongan_questions[question_number][3], font = ("Helvetica", "14"), bg = background_color, value = 3, variable = radiobutton_value, padx = 10, pady=10)
+
+        self.choice3.place(x=40, y=300)
+        #Radiobutton 4
+        self.choice4 = tk.Radiobutton (self, text = self.tongan_questions[question_number][4], font = ("Helvetica", "14"), bg = background_color, value = 4, variable = radiobutton_value, padx = 10, pady = 10)
+
+        self.choice4.place(x=40, y=350)
+
+        #Radiobutton 5
+        self.choice5 = tk.Radiobutton (self, text = self.tongan_questions[question_number][5], font = ("Helvetica", "14"), bg = background_color, value = 5, variable = radiobutton_value, padx = 10, pady = 10)
+
+        self.choice5.place(x=40, y=400)
+
+
+        #Confirm button
+        self.confirm_button = tk.Button (self, text = "Confirm", font = ("Helvetica", "13", "bold"), bg = background_color, command = self.test_progress)
+
+        self.confirm_button.grid (row = 6, padx = 5, pady = 5)
+
+        #Score label
+        self.score_label = tk.Label (self, text = "SCORE", font = ("Helvetica", "15"), bg = background_color,)
+
+        self.score_label.grid (row = 7, padx = 10, pady = 1)
+
+        #Quit Button
+        self.quit= tk.Button(self, text="Quit", font=("Helvetica", "13", "bold"), bg="IndianRed1", command=self.end_screen)
+        self.quit.place(x=40, y=550)
+
+        
+  
+          #Method showing the next questions data
+    def questions_setup (self):
+      radiobutton_value.set(0)
+      question_number +=1
+      self.question_label.config(text = self.tongan_questions[question_number][0])
+      self.choice1.config(text = self.tongan_questions[question_number][1])
+      self.choice2.config(text = self.tongan_questions[question_number][2])
+      self.choice3.config(text = self.tongan_questions[question_number][3])
+      self.choice4.config(text = self.tongan_questions[question_number][4])
+      self.choice5.config(text = self.tongan_questions[question_number][5])
+  
+    #This is the method that would get invoked with confrim answer button is clicked, to take care of test_progress
+  
+    def test_progress(self):
+      global score
+      scr_label = self.score_label
+      choice = radiobutton_value.get()
+      if len(asked)>9: #if the question is last
+        if choice == self.tongan_questions[question_number][6]: #if the last question is the correct answer
+          score+=1 
+          scr_label.configure(text = score)
+          self.confirm_button.config(text="Confirm")
+          self.end_screen()
+        else: #if the last question is the incorrect answer
+          print(choice)
+          score+=0
+          scr_label.configure(text = " The correct answer was " + self.tongan_questions[question_number][5])
+          self.confirm_button.config(text="Confirm")
+          self.end_screen()
+      else: #if it is not the last question
+        if choice==0: #checks if the user has made a choice
+          self.confirm_button.config(text="Try again please, you didnt select anyhting")
+          choice=radiobutton_value.get()
+        else: #if a choice was made and its not the last question
+          if choice == self.tongan_questions[question_number][6]: #if thier choice is correct
+            score+=1
+            scr_label.configure(text = score)
+            self.confirm_button.config(text="Confirm")
+            self.questions_setup() #execute the method to move on to the next question 
+          else: #if the choice was wrong
+            print(choice)
+            score+=0
+            scr_label.configure(text="The correct answer was: " + self.tongan_questions[question_number][5])
+            self.confirm_button.configure(text="Confirm")
+            self.questions_setup() 
+  
+  
+    def end_screen(self):
+        root.withdraw() 
 
  #starting point of the program and the controller of the display of different frames       
 class Program(tk.Tk):
